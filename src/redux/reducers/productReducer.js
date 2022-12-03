de187de0e0a7516e7223b1099e45f0a2b806cf47
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-12-03 15:11:08 +0600

    delete updated by quantity

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -12,53 +12,53 @@ const productReducer = (state = initialState, action) => {
   console.log(selectedProduct);
 
   switch (action.type) {
-    case ADD_TO_CART:
-       return {
-         ...state,
-         cart:[...state.cart, action.payload]
-       }
-       case REMOVE_FROM_CART:
-         return{
-           ...state,
-           cart: state.cart.filter((product)=>product._id !== action.payload._id)
-         }
+    // case ADD_TO_CART:
+    //    return {
+    //      ...state,
+    //      cart:[...state.cart, action.payload]
+    //    }
+    //    case REMOVE_FROM_CART:
+    //      return{
+    //        ...state,
+    //        cart: state.cart.filter((product)=>product._id !== action.payload._id)
+    //      }
   // }
   
-    // case ADD_TO_CART:
-    //   if (selectedProduct) {
-    //     const newCart = state.cart.filter(
-    //       (product) => product._id !== selectedProduct._id
-    //     );
+    case ADD_TO_CART:  
+      if (selectedProduct) {
+        const newCart = state.cart.filter(
+          (product) => product._id !== selectedProduct._id
+        );
 
-    //     selectedProduct.quantity = selectedProduct.quantity + 1;
+        selectedProduct.quantity = selectedProduct.quantity + 1;
 
-    //     return {
-    //       ...state,
-    //       cart: [...newCart, selectedProduct],
-    //     };
-    //   }
-    //   return {
-    //     ...state,
-    //     cart: [...state.cart, { ...action.payload, quantity: 1 }],
-    //   };
-    // case REMOVE_FROM_CART:
-    //   if (selectedProduct.quantity > 1) {
-    //     const newCart = state.cart.filter(
-    //       (product) => product._id !== selectedProduct._id
-    //     );
-    //     selectedProduct.quantity = selectedProduct.quantity - 1;
+        return {
+          ...state,
+          cart: [...newCart, selectedProduct],
+        };
+      }
+      return {
+        ...state,
+        cart: [...state.cart, { ...action.payload, quantity: 1 }],
+      };
+    case REMOVE_FROM_CART:
+      if (selectedProduct.quantity > 1) {
+        const newCart = state.cart.filter(
+          (product) => product._id !== selectedProduct._id
+        );
+        selectedProduct.quantity = selectedProduct.quantity - 1;
 
-    //     return {
-    //       ...state,
-    //       cart: [...newCart, selectedProduct],
-    //     };
-    //   }
-      // return {
-      //   ...state,
-      //   cart: state.cart.filter(
-      //     (product) => product._id !== action.payload._id
-      //   ),
-      // };
+        return {
+          ...state,
+          cart: [...newCart, selectedProduct],
+        };
+      }
+      return {
+        ...state,
+        cart: state.cart.filter(
+          (product) => product._id !== action.payload._id
+        ),
+      };
     default:
       return state;
   }
